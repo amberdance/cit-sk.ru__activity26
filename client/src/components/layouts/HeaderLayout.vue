f336d5115449c49bb4c1791b7799a462738000f2
Фишлер Алексей Иванович <ai.fishler@cit-sk.local>
2022-07-04 10:08:15 +0300

    Убрал ненужное, <a> заменен на <router-link>

--- client/src/components/layouts/HeaderLayout.vue
+++ client/src/components/layouts/HeaderLayout.vue
@@ -1,15 +1,22 @@
 <template>
   <div :class="$style.headerContent">
     <div :class="[$style.iconWrapper, 'd-flex align-center']">
-      <div :class="$style.item">icon1</div>
-      <el-divider direction="vertical" />
-      <div :class="$style.item">icon2</div>
+      <div :class="$style.item">
+        <router-link to="/">
+          <img src="../../assets/logo_primary.webp" alt="logo_primary" />
+        </router-link>
+      </div>
     </div>
 
     <div :class="[$style.authWrapper, 'd-flex align-center']">
-      <div :class="$style.item">Вход</div>
+      <div :class="$style.item">
+        <router-link to="/auth">Вход</router-link>
+      </div>
       <el-divider direction="vertical" />
-      <div :class="$style.item">Регистрация</div>
+
+      <div :class="$style.item">
+        <router-link to="/registration">Регистрация</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -22,12 +29,28 @@
   align-items: center;
   color: #ffffff;
   padding: 1rem;
-  min-height: 30px;
 }
 
 .authWrapper {
+  justify-content: center;
+  background-color: #ffffff;
+  border-radius: 50px;
+  height: 45px;
+  width: 180px;
+}
+
+.authWrapper a:hover {
+  color: #0469ff;
+  transition: 0.2s;
 }
 
 .iconWrapper {
+  background-color: #ffffff;
+  border-radius: 50px;
+  padding: 0px 15px;
+}
+
+img {
+  max-width: 45px !important;
 }
 </style>
