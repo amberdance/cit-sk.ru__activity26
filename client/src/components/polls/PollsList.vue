9be537b044042083d157f25f9d87117dc43c7b23
alexeevch <106554488+alexeevch@users.noreply.github.com>
2022-07-21 02:23:19 +0300

    Немного изменил стиль pollList

--- client/src/components/polls/PollsList.vue
+++ client/src/components/polls/PollsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background-color: #f5d299">
+  <div style="background-color: rgb(211 211 211)">
     <div class="container">
       <div :class="$style.polls_wrapper">
         <div :class="$style.heading">Опросы</div>
@@ -66,6 +66,7 @@ export default {
     try {
       this.isLoading = true;
       this.polls = await this.$http.get("/polls", { limit: 4 });
+      console.log(this.polls)
     } catch (e) {
       console.error(e);
     } finally {
@@ -90,6 +91,9 @@ export default {
   justify-content: center;
   width: 100%;
   margin: 0 !important;
+  background-color: #ebebeb;
+  padding: 20px;
+  border-radius: 15px;
 }
 .polls_card {
   background-color: #ffffff;
